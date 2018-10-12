a164810c0c8f29846240effbb2b22d4d8fa49ca7
陈长见 <ccj9@qq.com>
2018-10-12 10:15:47 +0800

    1

--- JSCardInfo_Category/JSCardInfo_Category/CTMediator+JSCardInfo.m
+++ JSCardInfo_Category/JSCardInfo_Category/CTMediator+JSCardInfo.m
@@ -8,16 +8,18 @@
 
 #import "CTMediator+JSCardInfo.h"
 NSString * const kCTMediatorTargetJSCardInfoCompleted = @"JSCardInfo";
+NSString * const kCTMediatorTargetJSCardInfoComplet = @"JSCardInfo";
 NSString * const kCTMediatorActionJSCardInfoViewControllerr = @"JSCardInfoViewController";
 
 @implementation CTMediator (JSCardInfo)
 
-- (UIViewController *)JSCardInfo_CardInfoViewControllerWithContentText:(NSString *)contentText{
+- (UIViewController *)JSCardInfo_JSCardInfoViewControllerWithContentText:(NSString *)contentText{
     /*
      BViewController *viewController = [[BViewController alloc] initWithContentText:@"hello, world!"];
      */
     NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
     params[@"contentText"] = contentText;
+    
     return [self performTarget:kCTMediatorTargetJSCardInfoCompleted action:kCTMediatorActionJSCardInfoViewControllerr params:params shouldCacheTarget:NO];
 }
 @end
